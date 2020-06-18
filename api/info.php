22b8428e7ec7ba43879b200d881cb54b37341b04
AuxiliumCDNG <47604750+AuxiliumCDNG@users.noreply.github.com>
2020-06-18 12:21:02 +0200

    fixed some mpc interaction bugs

--- api/info.php
+++ api/info.php
@@ -2,18 +2,26 @@
 $data = nl2br(shell_exec("mpc"));
 $bang = explode("\n", $data);
 
-$title = explode("<br", $bang[0])[0];
+if (count($bang) < 3) {
+    $title = "";
+    $time = "";
+    $player = $bang[0];
+    
+} else {
+    $title = explode("<br", $bang[0])[0];
 
-$stats = $bang[1];
-$part = explode("/", $stats)[1];
-$part = explode(" ", $part);
-$time = $part[count($part)-1];
+    $stats = $bang[1];
+    $part = explode("/", $stats)[1];
+    $part = explode(" ", $part);
+    $time = $part[count($part)-1];
 
-$player = $bang[2];
+    $player = $bang[2];
+}
+
 $part = explode("  ", $player)[0];
 $volume = explode(":", $part)[1];
 $volume = str_replace(" ", "", $volume);
 
-$res = array("title"=>$title,"time"=>$time,"volume"=>$volume);
+$res = array("title"=>urldecode($title),"time"=>$time,"volume"=>$volume);
 echo json_encode($res);
 ?>
